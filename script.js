a298e4e3d3d1ec2cdaab58da551a2f258a6116cf
SinKovac <71274524+SinKovac@users.noreply.github.com>
2020-11-03 09:23:33 -0500

    updated script
    added two more functions and rearranged code

--- script.js
+++ script.js
@@ -10,7 +10,25 @@ function startGame() {
     currentQuestionIndex = 0;
     questionContainerElement.classList.remove('hide');
     setNextQuestion();
+}    
+ function setNextQuestion() {
+    resetState();
+    showQuestion(currentQuestionIndex);
 }
+    
+ function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
+    })
+}   
 
 startBtn.addEventListener('click', startGame);
 nextBtn.addEventListener('click', () => {
