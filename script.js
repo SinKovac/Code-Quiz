8fd8ecce1981b116d236644e2c3c07d43320b77b
SinKovac <71274524+SinKovac@users.noreply.github.com>
2020-11-03 09:16:02 -0500

    created javascript
    added variables

---
+++ script.js
@@ -0,0 +1,3 @@
+var questionContainerElement = document.getElementById('question-container');
+var questionElement = document.getElementById('question');
+var answerButtonsElement = document.getElementById('answer-buttons');
