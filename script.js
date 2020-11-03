90fd7c86d05ab701435e4a8c2c292013b20ac35f
SinKovac <71274524+SinKovac@users.noreply.github.com>
2020-11-03 09:16:37 -0500

    updated javascript
    added more variables

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 var questionContainerElement = document.getElementById('question-container');
 var questionElement = document.getElementById('question');
 var answerButtonsElement = document.getElementById('answer-buttons');
+var startBtn = document.getElementById('start-btn');
+var nextBtn = document.getElementById('next-btn');
