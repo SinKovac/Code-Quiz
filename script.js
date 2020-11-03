5f7bddd0c27ee4bf89975a7e2690f407a561bce5
SinKovac <71274524+SinKovac@users.noreply.github.com>
2020-11-03 09:17:17 -0500

    updated javascript
    added variable

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ var questionElement = document.getElementById('question');
 var answerButtonsElement = document.getElementById('answer-buttons');
 var startBtn = document.getElementById('start-btn');
 var nextBtn = document.getElementById('next-btn');
+var gameClock = document.getElementById('game-clock');
+
